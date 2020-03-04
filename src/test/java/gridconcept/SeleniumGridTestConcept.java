3da8d8e838fe2d0dd437f1a3cd88bf088412daea
MOHAMMAD ROBIUL <md.rislam1980@gmail.com>
2020-03-04 12:17:34 -0500

    first round commit

---
+++ src/test/java/gridconcept/SeleniumGridTestConcept.java
@@ -0,0 +1,26 @@
+package gridconcept;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SeleniumGridTestConcept {
+
+	public static void main(String[] args) throws MalformedURLException {
+	DesiredCapabilities cap=DesiredCapabilities.chrome();
+	cap.setPlatform(Platform.WIN10);
+	URL url=new URL ("http://192.168.81.1:18297/wd/hub");
+	WebDriver driver=new RemoteWebDriver(url, cap);
+	driver.get("https://www.google.com");
+	System.out.println(driver.getTitle());
+	driver.close();
+	driver.quit();
+	
+	
+	}
+
+}
